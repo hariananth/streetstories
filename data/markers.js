9b0083e7237e0c5b0cd4a3994b402987d3a5ab90
Hari Ananth <hka2011@gmail.com>
2014-11-29 19:57:18 +0300

    split up data files so they're valid...

---
+++ data/markers.js
@@ -0,0 +1,18 @@
+var content = {
+  "pages": [
+    "lat": 38.73847,
+    "lng": -90.27387,
+    "markers": [
+      {
+        "img": "http://photos-g.ak.instagram.com/hphotos-ak-xpa1/10755974_611923265600446_759100302_n.jpg",
+        "lat": 38.73833,
+        "lng": -90.273545,
+        "title": "Michael Brown Memorial",
+        "content": ""+
+          "<iframe width='420' height='315'"+
+            "src='http://www.youtube.com/embed/XGSy3_Czz8k'>"+
+          "</iframe>",
+      }
+    ]
+  ]
+}
