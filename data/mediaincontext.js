301140ddfb7e57ca07d5b2928a3b0fdba90162d8
Rabble <evan@neo.com>
2014-12-01 02:48:35 +0300

    adding panoramas

---
+++ data/mediaincontext.js
@@ -0,0 +1,32 @@
+window.markerTypes = {
+  "family": "marker-fam",
+  "gov":    "marker-gov",
+  "news":   "marker-news",
+  "social": "marker-soc"
+};
+window.infoTypes = {
+  "picture": "info-picture",
+  "video":   "info-video",
+  "tweet":   "info-tweet"
+};
+
+window.mapInfo = [
+  {
+    "date": "August 9-10, 2014",
+    "lat": 25.3211176, // first lat-long is actually set by sosv (content.json)
+    "lng": 51.4366552, // so these aren't used
+    "markers": [
+      {
+        "date": "August 9, 2014",
+        "lat": 25.3211176,
+        "lng": 51.4366552, 
+        "headline": "On August 9th, 18-year-old Michael Brown was shot and killed",
+        "type": window.markerTypes.news,
+        "contentType": window.infoTypes.video,
+        "link": "",
+        "content": "6SGkD6CN0mk"
+      }
+    ]
+  }
+];
+
