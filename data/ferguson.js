8afd1f9e6057859004dda44ff15382abd23bd0c3
Hari Ananth <hka2011@gmail.com>
2014-12-01 03:45:56 +0300

    handling new json types

--- data/ferguson.js
+++ data/ferguson.js
@@ -15,6 +15,7 @@ window.mapInfo = [
     "date": "August 9-10, 2014",
     "lat": 38.738312, // first lat-long is actually set by sosv (content.json)
     "lng": -90.273611, // so these aren't used
+    "title": "",
     "markers": [
       {
         "date": "August 9, 2014",
@@ -22,7 +23,7 @@ window.mapInfo = [
         "lng": -90.273152, 
         "headline": "On August 9th, 18-year-old Michael Brown was shot and killed",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "6SGkD6CN0mk"
       },
@@ -32,7 +33,7 @@ window.mapInfo = [
         "lng": -90.279095,
         "headline": "",
         "type": window.markerTypes.family,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://scontent-a-ams.cdninstagram.com/hphotos-xpf1/l/t51.2885-15/1738085_692070914213393_1197767717_n.jpg"
       },
@@ -42,7 +43,7 @@ window.mapInfo = [
         "lng": -90.303086,
         "headline": "",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "zUXqPDT8hr4"
       },
@@ -52,6 +53,7 @@ window.mapInfo = [
     "date": "August 11, 2014",
     "lat": 38.749613,
     "lng": -90.279912,
+    "title": "",
     "markers": [
       {
         "date": "August 11, 2014",
@@ -59,7 +61,7 @@ window.mapInfo = [
         "lng": -90.279912,
         "headline": "Protests continue two days after Michael Brown's death",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "https://c4.staticflickr.com/4/3860/14794832317_db2b6eeb98_b.jpg"
       },
@@ -69,7 +71,7 @@ window.mapInfo = [
         "lng": -90.279912,
         "headline": "",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/6kah8zq.png"
       },
@@ -79,7 +81,7 @@ window.mapInfo = [
         "lng": -90.278376,
         "headline": "",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.tweet,
+        "infoType": window.infoTypes.tweet,
         "link": "",
         "content": '<blockquote class="twitter-tweet" lang="en"><p>It&#39;s just past 1 a.m. in <a href="https://twitter.com/hashtag/Ferguson?src=hash">#Ferguson</a>, and this is what we&#39;re seeing <a href="http://t.co/ZiKgGP2TGq">pic.twitter.com/ZiKgGP2TGq</a></p>&mdash; Alan Blinder (@alanblinder) <a href="https://twitter.com/alanblinder/status/500522932914114561">August 16, 2014</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
       },
@@ -89,6 +91,7 @@ window.mapInfo = [
     "date": "August 12, 2014",
     "lat": 38.648863,
     "lng": -90.338427,
+    "title": "",
     "markers": [
       {
         "date": "August 12, 2014",
@@ -96,7 +99,7 @@ window.mapInfo = [
         "lng": -90.338427,
         "headline": "",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.tweet,
+        "infoType": window.infoTypes.tweet,
         "link": "",
         "content": "<blockquote class='twitter-tweet' lang='en'><p>love this &lt;3 RT <a href='https://twitter.com/stevenjhsieh'>@stevenjhsieh</a>: Man waving a Palestinian flag in <a href='https://twitter.com/hashtag/Ferguson?src=hash'>#Ferguson</a> &quot;because they supported St. Louis.&quot; <a href='http://t.co/tn96pvFEXF'>pic.twitter.com/tn96pvFEXF</a></p>&mdash; Grim_Chickn (@grm_chikn) <a href='https://twitter.com/grm_chikn/status/500793766081744896'>August 16, 2014</a></blockquote><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>"
       },
@@ -106,7 +109,7 @@ window.mapInfo = [
         "lng": -90.338427,
         "headline": "",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "http://i.imgur.com/LsA3JUc.png"
       },
@@ -116,7 +119,7 @@ window.mapInfo = [
         "lng":  -90.277542,
         "headline": "Protest continue to rock Ferguson",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "qXhtvd0o2Fw"
       },
@@ -126,6 +129,7 @@ window.mapInfo = [
     "date": "August 13, 2014",
     "lat": 38.790223,
     "lng": -90.303040,
+    "title": "",
     "markers": [
       {
         "date": "August 13, 2014",
@@ -133,7 +137,7 @@ window.mapInfo = [
         "lng":  -90.303040,
         "headline": "",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "oTUqOBJsRdg"
       },
@@ -143,7 +147,7 @@ window.mapInfo = [
         "lng":  -90.303040,
         "headline": "First day of school in Ferguson-Florissant school district pushed back",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/AZ07MfO.png"
       },
@@ -153,7 +157,7 @@ window.mapInfo = [
         "lng":  -90.278376,
         "headline": "Two reporters detained by police at McDonald's",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "I6xmvDYCgKA"
       },
@@ -163,6 +167,7 @@ window.mapInfo = [
     "date": "August 14, 2014",
     "lat": 38.741005,
     "lng": -90.303095,
+    "title": "",
     "markers": [
       {
         "date": "August 14, 2014",
@@ -170,7 +175,7 @@ window.mapInfo = [
         "lng":  -90.303095,
         "headline": "",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "94SSU0CRg24"
       },
@@ -180,7 +185,7 @@ window.mapInfo = [
         "lng":  -90.303095,
         "headline": "",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/z761i8E.png"
       },
@@ -190,7 +195,7 @@ window.mapInfo = [
         "lng":  -90.234959,
         "headline": "Around the country, people hit the streets calling out policing in America",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/4GLvlAB.png"
       },
@@ -200,7 +205,7 @@ window.mapInfo = [
         "lng":  -90.234959,
         "headline": "And in Ferguson, protests raged another night",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "https://c1.staticflickr.com/9/8662/15885348561_1cf5208088_c.jpg"
       },
@@ -210,6 +215,7 @@ window.mapInfo = [
     "date": "August 15-18, 2014",
     "lat": 38.741005,
     "lng": -90.303095,
+    "title": "",
     "markers": [
       {
         "date": "August 15, 2014",
@@ -217,7 +223,7 @@ window.mapInfo = [
         "lng":  -90.303095,
         "headline": "",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://a.abcnews.com/images/US/HT_darren_wilson_float_jef_140819_16x9t_384.jpg"
       },
@@ -227,7 +233,7 @@ window.mapInfo = [
         "lng":  -90.303095,
         "headline": "",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "https://c2.staticflickr.com/6/5558/14978144321_1ff00e4617_b.jpg"
       },
@@ -237,7 +243,7 @@ window.mapInfo = [
         "lng":  -90.309098,
         "headline": "",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "2zspevpRgEU"
       },
@@ -247,7 +253,7 @@ window.mapInfo = [
         "lng":  -90.275085,
         "headline": "Nixon orders the National Guard into Ferguson",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.tweet,
+        "infoType": window.infoTypes.tweet,
         "link": "",
         "content": '<blockquote class="twitter-tweet" lang="en"><p>You can leave, but you can&#39;t come in ... <a href="https://twitter.com/hashtag/Ferguson?src=hash">#Ferguson</a> <a href="http://t.co/COawreJipm">pic.twitter.com/COawreJipm</a></p>&mdash; David Douglas (@DavidDouglasTV) <a href="https://twitter.com/DavidDouglasTV/status/500792925417377792">August 16, 2014</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
       },
@@ -257,6 +263,7 @@ window.mapInfo = [
     "date": "August 19, 2014",
     "lat": 38.669221,
     "lng": -90.273503,
+    "title": "",
     "markers": [
       {
         "date": "August 19, 2014",
@@ -264,7 +271,7 @@ window.mapInfo = [
         "lng":  -90.273503,
         "headline": "",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/H391N3Z.png"
       },
@@ -274,7 +281,7 @@ window.mapInfo = [
         "lng":  -90.273503,
         "headline": "On August 25th, Michael Brown was laid to rest.",
         "type": window.markerTypes.social,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "https://c2.staticflickr.com/6/5591/14841313420_6d555fdc4c_b.jpg"
       },
@@ -284,6 +291,7 @@ window.mapInfo = [
     "date": "September 16, 2014",
     "lat": 38.740754,
     "lng": -90.303037,
+    "title": "",
     "markers": [
       {
         "date": "September 16, 2014",
@@ -291,7 +299,7 @@ window.mapInfo = [
         "lng":  -90.303037,
         "headline": "Darren Wilson testifies to the grand jury",
         "type": window.markerTypes.gov,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/UCwPFyG.png"
       },
@@ -301,7 +309,7 @@ window.mapInfo = [
         "lng":  -90.275085,
         "headline": "Police in St. Louis chastised for wearing 'I am Darren Wilson' bracelets",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "https://c4.staticflickr.com/8/7473/15898608581_008aff9942_b.jpg"
       },
@@ -311,7 +319,7 @@ window.mapInfo = [
         "lng":  -90.275085,
         "headline": "",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://photos-g.ak.instagram.com/hphotos-ak-xap1/10724032_281371612061454_1935100233_n.jpg"
       }
@@ -321,6 +329,7 @@ window.mapInfo = [
     "date": "November 18-24, 2014",
     "lat": 38.710826, // first lat-long is actually set by sosv (content.json)
     "lng": -90.309098, // so these aren't used
+    "title": "",
     "markers": [
       {
         "date": "November 18, 2014",
@@ -328,7 +337,7 @@ window.mapInfo = [
         "lng": -90.309098,
         "headline": "Ferguson governor declares state of emergency ahead of grand jury decision",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.picture,
+        "infoType": window.infoTypes.picture,
         "link": "",
         "content": "http://i.imgur.com/wCj6CKq.png"
       },
@@ -338,7 +347,7 @@ window.mapInfo = [
         "lng": -90.309098,
         "headline": "",
         "type": window.markerTypes.news,
-        "contentType": window.infoTypes.tweet,
+        "infoType": window.infoTypes.tweet,
         "link": "",
         "content": '<blockquote class="twitter-tweet" lang="en"><p>Breaking News: No Indictment of Officer in Ferguson Case&#10;<a href="http://t.co/8f1zBwGHbV">http://t.co/8f1zBwGHbV</a></p>&mdash; The New York Times (@nytimes) <a href="https://twitter.com/nytimes/status/537070083982753792">November 25, 2014</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
       },
@@ -348,7 +357,7 @@ window.mapInfo = [
         "lng":-90.309098,
         "headline": "",
         "type": window.markerTypes.family,
-        "contentType": window.infoTypes.video,
+        "infoType": window.infoTypes.video,
         "link": "",
         "content": "h_1t-pQ2fx8"
       }
